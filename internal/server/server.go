3c7813cc8f4574792676c2440face3c57047ae6b
Joseph <kangbo.jk@gmail.com>
2020-10-01 16:21:25 -0700

    Add server

---
+++ internal/server/server.go
@@ -0,0 +1,66 @@
+package server
+
+import (
+	// "crypto/tls"
+
+	"net/http"
+	"time"
+
+	"github.com/kangbojk/go-react-fullstack/internal/server/router"
+	"github.com/kangbojk/go-react-fullstack/internal/usecase"
+)
+
+func NewServer(service usecase.Service) *http.Server {
+	handler := router.NewRouter(service)
+
+	s := &http.Server{
+		Addr:         ":8088",
+		Handler:      handler,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	return s
+}
+
+func Run() {
+}
+
+// func tlsConfig() *tls.Config {
+// 	crt, err := ioutil.ReadFile("./public.crt")
+// 	if err != nil {
+// 			log.Fatal(err)
+// 	}
+
+// 	key, err := ioutil.ReadFile("./private.key")
+// 	if err != nil {
+// 			log.Fatal(err)
+// 	}
+
+// 	cert, err := tls.X509KeyPair(crt, key)
+// 	if err != nil {
+// 			log.Fatal(err)
+// 	}
+
+// 	return &tls.Config{
+// 			Certificates: []tls.Certificate{cert},
+// 			ServerName:   "localhost",
+// 	}
+// }
+
+// func NewTLSServer() {
+// 	server := &http.Server{
+// 			Addr:         ":8443",
+// 			ReadTimeout:  5 * time.Second,
+// 			WriteTimeout: 10 * time.Second,
+// 			TLSConfig:    tlsConfig(),
+// 	}
+
+// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+// 			w.Write([]byte(fmt.Sprintf("Protocol: %s", r.Proto)))
+// 	})
+
+// 	if err := server.ListenAndServeTLS("", ""); err != nil {
+// 			log.Fatal(err)
+// 	}
+// }
