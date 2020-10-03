74568cc694d13e9f8167cd7549a68b054ba62253
Joseph <kangbo.jk@gmail.com>
2020-10-03 12:03:04 -0700

    Updates

---
+++ precache-manifest.9bfef3a4ae2d9dbe38ace31c49d06a56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c67c60d241ff3edeb3aded4bfda22b",
+    "url": "/kangbojk/go-react-fullstack/index.html"
+  },
+  {
+    "revision": "776c39f859463db61980",
+    "url": "/kangbojk/go-react-fullstack/static/css/main.1c3b4d4b.chunk.css"
+  },
+  {
+    "revision": "d0fb2048380149efb63b",
+    "url": "/kangbojk/go-react-fullstack/static/js/2.eaf24c0a.chunk.js"
+  },
+  {
+    "revision": "e57a44933d753860e139eb21ebb99237",
+    "url": "/kangbojk/go-react-fullstack/static/js/2.eaf24c0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "776c39f859463db61980",
+    "url": "/kangbojk/go-react-fullstack/static/js/main.f1bdfff3.chunk.js"
+  },
+  {
+    "revision": "7e83b1a2e2c932adddab",
+    "url": "/kangbojk/go-react-fullstack/static/js/runtime-main.ed02586e.js"
+  }
+]);
