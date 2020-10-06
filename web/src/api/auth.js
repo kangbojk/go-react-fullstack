857d8d199d4c0a46af5b13c86fb57cb6a56eb047
Joseph <kangbo.jk@gmail.com>
2020-10-06 12:02:59 -0700

    Add timeout for axios request

--- web/src/api/auth.js
+++ web/src/api/auth.js
@@ -3,5 +3,7 @@ import config from "../config"
 
 export const isLogin = () => {
     let apiUrl = config.url.api_auth_url // read from config    
-    return axios.get(apiUrl + 'account');
+    return axios.get(apiUrl + 'account', {
+        timeout: 700 // add timeout since wake up heroku free dyno during initial request take too long
+    });
 }
